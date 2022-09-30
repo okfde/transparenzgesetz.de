7796ec259419675f159461f2e1c85520699149f4
krmax44 <hi@krmax44.de>
2022-09-30 12:43:32 +0200

    🩹 fix base path for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import yaml from '@rollup/plugin-yaml';
 import icons from 'unplugin-icons/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  base: '/transparenzranking.de', // for github pages testing
   integrations: [
     tailwind({
       config: { applyBaseStyles: false }

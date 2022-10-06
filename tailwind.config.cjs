4780039738d53ef55bdc13e7171efa857a1f7d7c
krmax44 <hi@krmax44.de>
2022-10-06 10:12:44 +0200

    🐛 fix inter font not working

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans]
       }
     }
   },
